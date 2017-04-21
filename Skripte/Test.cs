ca253a10e604079c147d10c6bf62b5e2d430e796
1986cisco <cisco-sv@gmx.de>
2017-04-21 15:50:40 +0200

    TestFile zum Übertragen von Daten

---
+++ Skripte/Test.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Test : MonoBehaviour {
+
+
+    static UdpClient udp;
+    Thread thread;
+    static readonly object lockObject = new object();
+    string returnData = "";
+    bool precessData = false;
+
+    void Start()
+    {
+        //cubemove = cube.GetComponent<CubeMove>();
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
+    }
+
+    void Update()
+    {
+        if (precessData)
+        {
+            /*lock object to make sure there data is 
+             *not being accessed from multiple threads at thesame time*/
+            lock (lockObject)
+            {
+                precessData = false;
+               // cube.SendMessage("Move");
+                // or
+                //cubemove.Move();
+
+                //Process received data
+                Debug.Log("Received: " + returnData);
+
+                //Reset it for next read(OPTIONAL)
+                returnData = "";
+            }
+        }
+    }
+
+    private void ThreadMethod()
+    {
+        udp = new UdpClient(40);
+        while (true)
+        {
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] receiveBytes = udp.Receive(ref RemoteIpEndPoint);
+
+            /*lock object to make sure there data is 
+            *not being accessed from multiple threads at thesame time*/
+            lock (lockObject)
+            {
+                returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                Debug.Log(returnData);
+                if (returnData == "1\n")
+                {
+                    //Done, notify the Update function
+                    precessData = true;
+                }
+            }
+        }
+    }
+
+
+}
